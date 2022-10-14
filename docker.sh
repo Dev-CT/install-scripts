b70a02976f9402aa9d9c2212b90a87094a84fd13
CLOUDTECH <111585692+Dev-CT@users.noreply.github.com>
2022-10-14 15:09:24 -0500

    Install fix and change version print

--- docker.sh
+++ docker.sh
@@ -10,7 +10,7 @@ mkdir -p /etc/apt/keyrings
 
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
 
-echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu   $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 
 apt-get update -y
  
@@ -18,7 +18,7 @@ apt-get install -y docker-ce docker-ce-cli containerd.io docker-compose-plugin
 
 clear
 
-docker version
+docker - version
 docker compose version
 
 echo "Docker installed!"
